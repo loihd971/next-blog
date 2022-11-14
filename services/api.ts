22bab9252f683e45d753f063c465550cbcff2ea3
Loi Ha Dinh <loihad@nashtechglobal.com>
2022-11-14 11:11:05 +0700

    update

---
+++ services/api.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export async function loadPosts() {
+  try {
+    const res = await axios.get("http://localhost:3000/api/post");
+    return res;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+}
+
+export const get = (url: string) => axios.get(url).then((res) => res.data);
